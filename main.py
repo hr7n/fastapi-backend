a4c7b6005021f8137827347f11fbed96e3ccf5e3
mhorton <michaelhorton722@gmail.com>
2024-02-28 18:25:51 -0500

    adds function to initialize db

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+from database import engine
+from models import User, Loan
 
 app = FastAPI()
 
@@ -6,3 +9,11 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Test"}
+
+
+def create_db():
+    SQLModel.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    create_db()
