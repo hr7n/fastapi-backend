e8b1f1bce02a38bbb8a40fbdffbbbf437b49ad4f
mhorton <horton-michael@users.noreply.github.com>
2024-02-29 14:10:47 -0500

    adds routes to create user and get user by id

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from fastapi import FastAPI
-from database import engine, SQLModel
+from fastapi import FastAPI, Depends, HTTPException
+from database import engine, get_session
+from sqlmodel import SQLModel, Session
 from models import User, Loan
 
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"message": "Test"}
+# @app.get("/")
+# async def read_root():
+#     return {"message": "Test"}
+
+
+@app.post("/users/", response_model=User)
+def create_user(user: User, db: Session = Depends(get_session)):
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+@app.get("/users/{user_id}", response_model=User)
+def read_user(user_id: int, db: Session = Depends(get_session)):
+    user = db.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
 
 def create_db():
