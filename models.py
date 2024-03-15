7ffb8d2bd5d03f43588079ed2125bcf3f6fb91a0
mhorton <horton-michael@users.noreply.github.com>
2024-03-15 13:20:11 -0400

    adds route to share a loan with another user

--- models.py
+++ models.py
@@ -1,12 +1,15 @@
 from typing import Optional, List
 from sqlmodel import SQLModel, Field, Relationship
 
+class UserLoanLink(SQLModel, table=True):
+    user_id: Optional[int] = Field(default=None, foreign_key='user.id', primary_key=True)
+    loan_id: Optional[int] = Field(default=None, foreign_key='loan.id', primary_key=True)
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     email: str = Field(index=True, sa_column_kwargs={"unique": True})
-    loans: List["Loan"] = Relationship(back_populates="owner", link_model=JoinedUserLoans)
+    loans: List["Loan"] = Relationship(back_populates="owner", link_model=UserLoanLink)
 
 
 class Loan(SQLModel, table=True):
@@ -15,7 +18,7 @@ class Loan(SQLModel, table=True):
     amount: float
     annual_interest_rate: float
     loan_term_months: int
-    owner: List[User] = Relationship(back_populates="loans", link_model=JoinedUserLoans)
+    owner: List[User] = Relationship(back_populates="loans", link_model=UserLoanLink)
 
 
 class LoanSchedule(SQLModel):
@@ -27,7 +30,3 @@ class LoanSummary(SQLModel):
     total_principal_paid: float
     total_interest_paid: float
     current_principal_balance: float
-
-class JoinedUserLoans(SQLModel, table=True):
-    user_id: Optional[int] = Field(default=None, foreign_key='user.id', primary_key=True)
-    loan_id: Optional[int] = Field(default=None, foreign_key='loan.id', primary_key=True)
