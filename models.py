a52353c243251db57b84cbaa7d9f7bf0f004e509
mhorton <horton-michael@users.noreply.github.com>
2024-03-15 12:22:15 -0400

    adds joined table to create many-to-many relationship between Users and Loans

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     email: str = Field(index=True, sa_column_kwargs={"unique": True})
-    loans: List["Loan"] = Relationship(back_populates="owner")
+    loans: List["Loan"] = Relationship(back_populates="owner", link_model=JoinedUserLoans)
 
 
 class Loan(SQLModel, table=True):
@@ -15,7 +15,7 @@ class Loan(SQLModel, table=True):
     amount: float
     annual_interest_rate: float
     loan_term_months: int
-    owner: User = Relationship(back_populates="loans")
+    owner: List[User] = Relationship(back_populates="loans", link_model=JoinedUserLoans)
 
 
 class LoanSchedule(SQLModel):
@@ -27,3 +27,7 @@ class LoanSummary(SQLModel):
     total_principal_paid: float
     total_interest_paid: float
     current_principal_balance: float
+
+class JoinedUserLoans(SQLModel, table=True):
+    user_id: Optional[int] = Field(default=None, foreign_key='user.id', primary_key=True)
+    loan_id: Optional[int] = Field(default=None, foreign_key='loan.id', primary_key=True)
